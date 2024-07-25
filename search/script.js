0011ceac35477982e284a360222ecd7d84e3b95d
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-25 19:45:17 +0500

    Boyer Moore algorithm

---
+++ search/script.js
@@ -0,0 +1,55 @@
+function generateRandomNumbers(count, min, max) {
+    const randomNumbers = [];
+    for (let i = 0; i < count; i++) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        randomNumbers.push(randomNumber);
+    }
+    return randomNumbers;
+}
+console.time("search");
+function preprocessBadCharacterShift(pattern) {
+    const m = pattern.length;
+    const badCharShift = new Map();
+    
+    for (let i = 0; i < m - 1; i++) {
+        badCharShift.set(pattern[i], m - i - 1);
+    }
+
+    return badCharShift;
+}
+function boyerMoore(array, pattern) {
+    const n = array.length;
+    const m = pattern.length;
+    if (m === 0) {
+        return [];
+    }
+    const badCharShift = preprocessBadCharacterShift(pattern);
+    const matches = [];
+
+    let s = 0;
+    while (s <= n - m) {
+        let j = m - 1;
+
+        while (j >= 0 && pattern[j] === array[s + j]) {
+            j--;
+        }
+        if (j < 0) {
+            matches.push(s);
+            s += (s + m < n) ? m - (badCharShift.get(array[s + m]) || m) : 1;
+        } else {
+            s += Math.max(1, j - (badCharShift.get(array[s + j]) || m));
+        }
+    }
+    return matches;
+}
+console.timeEnd("search");
+const randomNumbers = generateRandomNumbers(1000000, 0, 1000000);
+console.log("Generated Random Numbers:", randomNumbers);
+const userInput ="26, 23";
+const pattern = userInput.split(",").map(Number);
+const result = boyerMoore(randomNumbers, pattern);
+if (result.length > 0) {
+    console.log(`Pattern found at positions: ${result}`);
+} else {
+    console.log("Pattern not found in the array.");
+}
